6b8f0f3ed0c01083b3b3dae82f99c9c75850bf64
Manav Panchal <manavpanchal1223@gmail.com>
2025-04-28 21:26:20 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/flavorverse-recipe-blueprint' : '/'}>
+      <BrowserRouter basename={ '/'}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/recipes/:id" element={<RecipePage />} />
