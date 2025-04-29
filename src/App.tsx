4e55277976984d0a8f09d0f1bb403c8c3e127fe1
Manav Panchal <manavpanchal1223@gmail.com>
2025-04-28 21:40:09 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import RecipePage from "./pages/RecipePage";
 import CategoryPage from "./pages/CategoryPage";
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/flavorverse-recipe-blueprint' : '/'}>
+      <HashRouter basename={import.meta.env.MODE === 'production' ? '/flavorverse-recipe-blueprint' : '/'}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/recipes/:id" element={<RecipePage />} />
@@ -33,7 +33,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
